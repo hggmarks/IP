4baf9edab7b049580216fa0e452093b0a96e54ae
hggmarks <hggmarks@gmail.com>
2022-03-09 18:00:32 -0300

    add: questão 6 lista 2 finalizada

---
+++ lista-2/prova_do_lider.py
@@ -0,0 +1,42 @@
+# letra = input()
+# numero = int(input())
+letra = input()
+numero = int(input()) 
+
+if letra == 'X' and numero % 2 == 1:
+    for i in range(numero):
+        for j in range(numero):
+            if j == 0+i or j == numero-i-1:
+                print('X', end='')
+            else:
+                print('0', end='')
+        if i != numero-1:
+            print(end="\n\n")
+        else:
+            print()
+elif letra == 'T' and numero % 2 == 1:
+
+    for i in range(numero):
+        for j in range(numero):
+            if i == 0 or j == int(numero/2):
+                print('X', end='')
+            else:
+                print('0', end='')
+        if i != numero-1:
+            print(end="\n\n")
+        else:
+            print()
+elif letra == 'O' and numero % 2 == 1:
+    
+    for i in range(numero):
+        for j in range(numero):
+            if i == 0 or i == numero-1 or j == 0 or j == numero-1:
+                print('X', end='')
+            else:
+                print('0', end='')
+        if i != numero-1:
+            print(end="\n\n")
+        else:
+            print()
+else:
+    print('Entrada não permitida')
