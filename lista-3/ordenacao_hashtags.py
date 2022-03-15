07e4f6f0557fac36fe0fa5295e12a45805764269
Hugo Marques <hfm@discente.ifpe.edu.br>
2022-03-15 07:09:11 +0000

    ad: questão 5 lista 3 finalizadaa

---
+++ lista-3/ordenacao_hashtags.py
@@ -0,0 +1,24 @@
+qtd_hashtags = int(input())
+hashtags = []
+flag = False
+
+while qtd_hashtags > 0:
+    hashtag = input()
+    for character in hashtag:
+        if ord(character) >= 65 and ord(character) <= 90:
+            flag = True
+            
+    if not flag:
+        hashtags.append(hashtag)
+        
+    flag = False
+    
+    qtd_hashtags -= 1
+
+for hashtag in hashtags:
+    for index in range(len(hashtags)-1):
+        if hashtags[index] < hashtags[index+1]:
+            hashtags[index], hashtags[index+1] = hashtags[index+1], hashtags[index]
+
+for item in hashtags:
+    print(item)
