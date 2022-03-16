a8c9c700af14241651785216af13d13590b19153
Hugo Marques <hfm@discente.ifpe.edu.br>
2022-03-16 18:42:36 +0000

    add: questao 7 lista 3

---
+++ lista-3/cover_billy_ray.py
@@ -0,0 +1,17 @@
+contador  = 10
+frases_original = []
+frases_limpas = []
+all_noise = True
+no_noise = False
+while contador > 0:
+    frases_original.append(input())
+    contador -= 1
+
+
+for frase in frases_original:
+    if not (frase == 'tss' or frase == 'zzz'):
+        all_noise = False
+    elif :
+        
+if all_noise:
+    print('Eita, a legenda simplesmente inexiste! Tudo era ruido!')
