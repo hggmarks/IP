9cc0cdbb2c144d66aacf517c7926e9e600c746a0
Hugo Marques <hfm@discente.ifpe.edu.br>
2022-03-16 20:01:56 +0000

    add: questao 7 lista 3 finalizada

--- lista-3/cover_billy_ray.py
+++ lista-3/cover_billy_ray.py
@@ -1,17 +1,47 @@
 contador  = 10
 frases_original = []
 frases_limpas = []
-all_noise = True
-no_noise = False
+noise = True
+num_ruidos = 0
 while contador > 0:
     frases_original.append(input())
     contador -= 1
 
 
+
+
 for frase in frases_original:
     if not (frase == 'tss' or frase == 'zzz'):
-        all_noise = False
-    elif :
+        noise = False
+    
+    if (frase[:3] == 'tss' or frase[:3] == 'zzz') and (frase[-3:] == 'tss' or frase[-3:] == 'zzz'):
+        frases_limpas.append(frase[3:-3].strip())
+        num_ruidos += 2
+    elif frase[:3] == 'tss' or frase[:3] == 'zzz':
+        frases_limpas.append(frase[3:].strip())
+        num_ruidos += 1
+    elif frase[-3:] == 'tss' or frase[-3:] == 'zzz':
+        frases_limpas.append(frase[:-3].strip())
+        num_ruidos += 1
+    elif ('tss' not in frase) and ('zzz' not in frase):
+        frases_limpas.append(frase.strip())
+
+for item in frases_limpas.copy():
+    if item == '':
+        frases_limpas.remove('')
+
         
-if all_noise:
+if noise:
     print('Eita, a legenda simplesmente inexiste! Tudo era ruido!')
+else:
+    print('Legenda final:\n')
+    
+    print(*frases_limpas, sep='\n')
+    print()
+
+    if (num_ruidos >= 1) and (num_ruidos <= 4) and (len(frases_limpas) >= 8):
+        print('Todo o ruido foi removido e voce mandou bem! A legenda saiu certinha. Pode subir!')
+    elif num_ruidos == 0:
+        print('Nem precisava rodar, o audio ja estava limpinho e a legenda ta nos conformes. Marca o @billyraycyrus')
+    elif num_ruidos > 4 or len(frases_limpas) < 8:
+        print('Ih, tem alguma coisa errada com a legenda, ta estranha. Melhor dar uma verificada e regravar.')
