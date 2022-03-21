dd1a2ad8b36f7af1863e42ff98ad08a498b45853
Hugo Marques <hfm@discente.ifpe.edu.br>
2022-03-21 20:00:49 +0000

    update: questao 10 lista 3 nao finalizada

--- lista-3/time_do_samuca.py
+++ lista-3/time_do_samuca.py
@@ -31,13 +31,13 @@ while entrada != 'fim':
             break
         
         #verificando mais de 5 jogadores por posição
-        if len(levantador) > 5:
+        if len(levantador) == 5:
             print('Cuidado! voce ja possui 5 levantadores')
-        if len(central) > 5:
+        if len(central) == 5:
             print('Cuidado! voce ja possui 5 centrais')
-        if len(ponteiro) > 5:
+        if len(ponteiro) == 5:
             print('Cuidado! voce ja possui 5 ponteiros')
-        if len(oposto) > 5:
+        if len(oposto) == 5:
             print('Cuidado! voce ja possui 5 opostos')
         
         if (len(levantador) + len(central) + len(ponteiro) + len(oposto) + len(libero)) > 18:
@@ -45,9 +45,21 @@ while entrada != 'fim':
             break
     
     elif entrada == 'relatorio':
+        total = len(libero)+len(levantador)+len(ponteiro)+len(central)+len(oposto)
         print('No nosso time ja possuimos:')
         print(f'Liberos: {len(libero)}')
         print(f'Levantadores: {len(levantador)}')
-        
-    elif entrada == 'nomes':
+        print(f'Ponteiros: {len(ponteiro)}')
+        print(f'Centrais: {len(central)}')
+        print(f'Opostos: {len(oposto)}')
+        print(f'TOTAL: {total}')
     elif entrada == 'buscar':
+        
+    elif entrada == 'nomes': 
+        operacao = input()
+        if operacao == 'libero':
+            if libero != []:
+                print(f'Os liberos são:')
+                print(*libero, sep='\n')
+            else: 
+                print('Ainda nao temos jogadores nessa posicao')
