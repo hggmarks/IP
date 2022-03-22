2dfacb389f793d7bc1c841da560409f64bf4282f
hggmarks <hggmarks@gmail.com>
2022-03-22 00:03:16 -0300

    update: questão 10 lista 3 não finalizada

--- lista-3/time_do_samuca.py
+++ lista-3/time_do_samuca.py
@@ -53,13 +53,57 @@ while entrada != 'fim':
         print(f'Centrais: {len(central)}')
         print(f'Opostos: {len(oposto)}')
         print(f'TOTAL: {total}')
+
     elif entrada == 'buscar':
+        operacao = input()
+        if (operacao in libero) or (operacao in levantador) or (operacao in central) or (operacao in ponteiro) or (operacao in oposto):
+            if operacao == 'Samuel':
+                print('Sim, Samuel, voce ja esta na lista de jogadores')
+            else:
+                print(f'Sim, {operacao} esta na lista de jogadores')
+        else:
+            if operacao == 'Samuel':
+                print('Como voce pode esquecer de si mesmo? Voce nao esta na lista')
+            else:
+                print(f'O jogador {operacao} nao esta na lista de jogadores')
         
     elif entrada == 'nomes': 
         operacao = input()
-        if operacao == 'libero':
+        if operacao == 'Libero':
             if libero != []:
-                print(f'Os liberos são:')
+                print('Os liberos sao:')
                 print(*libero, sep='\n')
             else: 
                 print('Ainda nao temos jogadores nessa posicao')
+        elif operacao == 'Levantador':
+            if levantador != []:
+                print('Os levantadores sao:')
+                print(*levantador, sep='\n')
+            else:
+                print('Ainda nao temos jogadores nessa posicao')
+        elif operacao == 'Central':
+            if central != []:
+                print('Os centrais sao:')
+                print(*central, sep='\n')
+            else:  
+                print('Ainda nao temos jogadores nessa posicao')
+        elif operacao == 'Ponteiro':
+            if ponteiro != []:
+                print('Os ponteiros sao:')
+                print(*ponteiro, sep='\n')
+            else:
+                print('Ainda nao temos jogadores nessa posicao')
+        elif operacao == 'Oposto':
+            if  oposto != []:
+                print('Os opostos sao:')
+                print(*oposto, sep='\n')
+            else:
+                print('Ainda nao temos jogadores nessa posicao')
+    else:
+        print('***COMANDO INVALIDO***')
+        
+
+if(len(libero) == 2 and len(libero)+len(levantador)+len(ponteiro)+len(central)+len(oposto) <= 18):
+    print(f'O Navegantes esta pronto para disputar o Estadual! Desejem sorte aos nossos {len(libero)+len(levantador)+len(ponteiro)+len(central)+len(oposto)} jogadores!')
+else:
+    print('Quem mandou ficar perdendo tempo com TikTok... Agora o Navegantes nao conseguira jogar o estadual :(')
