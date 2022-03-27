3eadc69072d28801808d62e63fed96e9bd3e332e
hggmarks <hfm@discente.ifpe.edu.br>
2022-03-27 17:29:43 -0300

    add: questao 0 lista 4 finalizada

--- lista-4/L4D2.py
+++ lista-4/L4D2.py
@@ -5,11 +5,15 @@ def qtd_zumbis(rodada: int, kills=0, vivos=0):
 if __name__ == '__main__':
     total_rodadas = int(input())
     rodada = 1
-    while rodada < total_rodadas:
-        n_kills, n_vivos = input().split('-')
-        n_kills = int(n_kills.strip())
-        n_vivos = int(n_vivos.strip())
+    while rodada <= total_rodadas:
+        if rodada == 1:
+            print(f'Haverá {qtd_zumbis(rodada)} inimigos na rodada {rodada}')
+        else:
 
-        print(f'Haverá {qtd_zumbis(rodada, n_kills, n_vivos)} inimigos na rodada {rodada}')
+            n_kills, n_vivos = input().split(' - ')
+            n_kills = int(n_kills)
+            n_vivos = int(n_vivos)
+
+            print(f'Haverá {qtd_zumbis(rodada, n_kills, n_vivos)} inimigos na rodada {rodada}')
         rodada += 1
         
