cd234770047a41332e30f8105be1b62a8e6845d7
Hugo Marques <hfm@discente.ifpe.edu.br>
2022-04-13 20:07:54 +0000

    update: questao 5 em andamento

--- lista-5/run_link_run.py
+++ lista-5/run_link_run.py
@@ -1,14 +1,49 @@
+def print_find_zelda():
+    return print('Vamos embora daqui Princesa!!!')
+
+def print_map(map: list):
+    return print(*map, sep='\n')
+
 def move_link(map: list, coord_link: list, matrix_size: int):
-    link_char = 'L'
-    if map[coord_link[0]+1][coord_link[1]] == '.':
-        map[coord_link[0]+1][coord_link[1]] = link_char
+    link_char =  'L'
+    zelda_char = 'Z'
 
-    elif map[coord_link[0]][coord_link[1]+1] == '.':
-        map[coord_link[0]][coord_link[1]+1] = link_char
+    if (coord_link[1] < matrix_size-1) and ((map[coord_link[0]+1][coord_link[1]] == '.') or (map[coord_link[0]+1][coord_link[1]] == zelda_char)):
+        #baixo
+        if (map[coord_link[0]+1][coord_link[1]] == zelda_char):
+            map[coord_link[0]+1][coord_link[1]] = link_char
+            print_map(map)
+            return print_find_zelda()
+        else:
+            map[coord_link[0]+1][coord_link[1]] = link_char
+            print_map(map)
+            return move_link(map, coord_link, matrix_size)
 
-    elif map[coord_link[0]-1][coord_link[1]] == '.':
-        map[coord_link[0]-1][coord_link[1]] = link_char
+    elif (coord_link[0] < matrix_size-1) and ((map[coord_link[0]][coord_link[1]+1] == '.') or (map[coord_link[0]][coord_link[1]+1] == zelda_char)):
+        #direita
+        if (map[coord_link[0]][coord_link[1]+1] == zelda_char):
+            map[coord_link[0]][coord_link[1]+1] = link_char
+            return print_find_zelda()
+        else:
+            map[coord_link[0]][coord_link[1]+1] = link_char
+            return move_link(map, coord_link, matrix_size)
+        
 
+    elif (coord_link[1] > 0) and ((map[coord_link[0]-1][coord_link[1]] == '.') or (map[coord_link[0]-1][coord_link[1]] == zelda_char)):
+        #cima
+        if (map[coord_link[0]-1][coord_link[1]] == zelda_char):
+            map[coord_link[0]-1][coord_link[1]] = link_char
+            return print_find_zelda()
+        else:
+            map[coord_link[0]-1][coord_link[1]] = link_char
+
+    elif coord_link[0] > 0 and map[coord_link[0]][coord_link[1]-1] == '.':
+        #esquerda
+        map[coord_link[0]][coord_link[1]-1] = link_char
+    else:
+        print('HAHAHAHA você nunca irá resgatá-la, Link!!!')
+        
+    
 
 
 
