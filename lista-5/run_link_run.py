7bee49c885b1e00f6dfc46a641e798c44c3704e4
hggmarks <hggmarks@gmail.com>
2022-04-14 00:07:54 -0300

    update: lista 5 questão 5 finalizada

--- lista-5/run_link_run.py
+++ lista-5/run_link_run.py
@@ -2,7 +2,8 @@ def print_find_zelda():
     return print('Vamos embora daqui Princesa!!!')
 
 def print_map(map: list):
-    return print(*map, sep='\n')
+    for item in map:
+        print(''.join(item))
 
 def move_link(map: list, coord_link: list, matrix_size: int):
     link_char =  'L'
@@ -12,20 +13,29 @@ def move_link(map: list, coord_link: list, matrix_size: int):
         #baixo
         if (map[coord_link[0]+1][coord_link[1]] == zelda_char):
             map[coord_link[0]+1][coord_link[1]] = link_char
+            coord_link[0] += 1
             print_map(map)
             return print_find_zelda()
+
         else:
             map[coord_link[0]+1][coord_link[1]] = link_char
+            coord_link[0] += 1
             print_map(map)
+            print('Caminharei pelo Sul e certamente irei encontrar-te, Zelda')
             return move_link(map, coord_link, matrix_size)
 
     elif (coord_link[0] < matrix_size-1) and ((map[coord_link[0]][coord_link[1]+1] == '.') or (map[coord_link[0]][coord_link[1]+1] == zelda_char)):
         #direita
         if (map[coord_link[0]][coord_link[1]+1] == zelda_char):
             map[coord_link[0]][coord_link[1]+1] = link_char
+            coord_link[1] += 1
+            print_map(map)
             return print_find_zelda()
         else:
             map[coord_link[0]][coord_link[1]+1] = link_char
+            coord_link[1] += 1
+            print_map(map)
+            print('Caminharei pelo Leste e certamente irei encontrar-te, Zelda\n')
             return move_link(map, coord_link, matrix_size)
         
 
@@ -33,19 +43,32 @@ def move_link(map: list, coord_link: list, matrix_size: int):
         #cima
         if (map[coord_link[0]-1][coord_link[1]] == zelda_char):
             map[coord_link[0]-1][coord_link[1]] = link_char
+            coord_link[0] -= 1
+            print_map(map)
             return print_find_zelda()
         else:
             map[coord_link[0]-1][coord_link[1]] = link_char
+            coord_link[0] -= 1
+            print_map(map)
+            print('Caminharei pelo Norte e certamente irei encontrar-te, Zelda\n')
+            return move_link(map, coord_link, matrix_size) 
 
-    elif coord_link[0] > 0 and map[coord_link[0]][coord_link[1]-1] == '.':
+    elif (coord_link[0] > 0) and ((map[coord_link[0]][coord_link[1]-1] == '.') or (map[coord_link[0]][coord_link[1]-1] == zelda_char)):
+        if (map[coord_link[0]][coord_link[1]-1] == zelda_char):
+            map[coord_link[0]][coord_link[1]-1] = link_char
+            coord_link[1] -= 1
+            print_map(map)
+            print('Caminharei pelo Oeste e certamente irei encontrar-te, Zelda\n')
+            return print_find_zelda()
+        else:
         #esquerda
-        map[coord_link[0]][coord_link[1]-1] = link_char
+            map[coord_link[0]][coord_link[1]-1] = link_char
+            coord_link[1] -= 1
+            print_map(map)
+            return move_link(map, coord_link, matrix_size)
     else:
         print('HAHAHAHA você nunca irá resgatá-la, Link!!!')
         
-    
-
-
 
 if __name__ == '__main__':
     matrix_size = int(input())
@@ -56,5 +79,5 @@ if __name__ == '__main__':
         mapa.append(list(input()))
         counter -= 1
     
-    print(*mapa, sep='\n')
-        
+    
+    move_link(mapa, player_pos, matrix_size)
